e39eb976818f970ee9efd30530b8fa6898509868
doctorzalman <georgesun30@gmail.com>
2021-08-19 21:07:58 +0200

    page: authors, about us, partners, main

--- wp-content/themes/urbanspaceradio/template-parts/blocks/block_aboutUSR.php
+++ wp-content/themes/urbanspaceradio/template-parts/blocks/block_aboutUSR.php
@@ -1,21 +1,21 @@
 <?php
 defined( 'ABSPATH' ) || exit;
-$Block_aboutUSR = getBlock_aboutUSR();
+$Block_aboutUSR_1 = getBlock_aboutUSR();
 ?>
 <section>
   <div class="siteWrapper">
     <div class="aboutUSR">
       <div class="aboutUSR-header">
         <div class="aboutUSR-header_text">
-          <h1><?php echo $Block_aboutUSR['aboutUSR-title']; ?></h1>
-          <p><?php echo $Block_aboutUSR['aboutUSR-desc']; ?></p>
+          <h1><?php echo $Block_aboutUSR_1['aboutUSR-title']; ?></h1>
+          <p><?php echo $Block_aboutUSR_1['aboutUSR-desc']; ?></p>
         </div>
         <div class="aboutUSR-header_img">
-          <img alt="aboutUSR" src="<?php echo $Block_aboutUSR['aboutUSR-img']; ?>">
+          <img alt="aboutUSR" src="<?php echo $Block_aboutUSR_1['aboutUSR-img']; ?>">
         </div>
       </div>
       <div class="aboutUSR-mainText">
-        <p><?php echo $Block_aboutUSR['aboutUSR-text']; ?></p>
+        <p><?php echo $Block_aboutUSR_1['aboutUSR-text']; ?></p>
       </div>
       <div class="btn-green btn-green-main">
         <a href="#">підтримати проект</a>
