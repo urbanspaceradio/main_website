f768dc8f9d70fcd9413596f954e6df4ad390e8f5
doctorzalman <georgesun30@gmail.com>
2021-10-08 17:51:11 +0200

    add facebook pixel to header.php

--- wp-content/themes/urbanspaceradio_min/header.php
+++ wp-content/themes/urbanspaceradio_min/header.php
@@ -15,6 +15,23 @@ defined('ABSPATH') || exit;
 		  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	  })(window,document,'script','dataLayer','GTM-TZSH5JW');</script>
   <!-- End Google Tag Manager -->
+  <!-- Facebook Pixel Code -->
+  <script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		  n.queue=[];t=b.createElement(e);t.async=!0;
+		  t.src=v;s=b.getElementsByTagName(e)[0];
+		  s.parentNode.insertBefore(t,s)}(window, document,'script',
+		  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '349752110169807');
+	  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+                 src="https://www.facebook.com/tr?id=349752110169807&ev=PageView&noscript=1"
+    /></noscript>
+  <!-- End Facebook Pixel Code -->
 	<meta content="urban подкасти, актуальні подкасти, свіжі подкасти, урбан подкасти, подкасти українською, подкасти на українській, подкасти українським, подкасти на українском язике, подкасти слухати, подкасти на google podcasts, подкасти apple podcasts, google podcasts, apple podcasts, топ подкасти україни, цікаві подкасти, подкасти це, подкасті, найкращі українські подкасти, підкасти це, де слухати подкасти, подкасти слухати, сайт з подкастами, аудіо книги скачать, що таке подкаст, аудіо книги, google подкасти, подкасти про психологію, легкі подкасти, urban space radio, urban space 100, USR, урбан радіо" name="keywords">
   <meta content="Радіо нового формату, в ефірі якого звучить українська сучасна музика і програми на тему урбаністики та розвитку міста" name="description">
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
