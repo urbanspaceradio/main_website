d1353955599433937fdff09cc645c572c2ec0c6c
doctor_zalman <georgesun30@gmail.com>
2022-02-14 15:45:00 +0200

    fix bugs and correcting some styles on single-events.php

--- wp-content/themes/urbanspaceradio_min/page-player.php
+++ wp-content/themes/urbanspaceradio_min/page-player.php
@@ -45,6 +45,7 @@
 	crossorigin="anonymous"
 	integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
 	src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.jplayer.min.js"
 		  type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/player.js" type="text/javascript"></script>
