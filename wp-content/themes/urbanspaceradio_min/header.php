f27d3912b403ba1342fb3d8dc40474619dd10e00
doctor_zalman <georgesun30@gmail.com>
2022-10-04 11:27:47 +0300

    feat(Header): add Pixel Google

--- wp-content/themes/urbanspaceradio_min/header.php
+++ wp-content/themes/urbanspaceradio_min/header.php
@@ -15,23 +15,7 @@ defined('ABSPATH') || exit;
 		  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	  })(window,document,'script','dataLayer','GTM-TZSH5JW');</script>
   <!-- End Google Tag Manager -->
-  <!-- Facebook Pixel Code -->
-  <script>
-	  !function(f,b,e,v,n,t,s)
-	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-		  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-		  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-		  n.queue=[];t=b.createElement(e);t.async=!0;
-		  t.src=v;s=b.getElementsByTagName(e)[0];
-		  s.parentNode.insertBefore(t,s)}(window, document,'script',
-		  'https://connect.facebook.net/en_US/fbevents.js');
-	  fbq('init', '349752110169807');
-	  fbq('track', 'PageView');
-  </script>
-  <noscript><img height="1" width="1" style="display:none"
-                 src="https://www.facebook.com/tr?id=349752110169807&ev=PageView&noscript=1"
-    /></noscript>
-  <!-- End Facebook Pixel Code -->
+
   <meta name="facebook-domain-verification" content="83zs5sacudrn4p3hqpgr5bgr20p5mc" />
 	<meta content="urban подкасти, актуальні подкасти, свіжі подкасти, урбан подкасти, подкасти українською, подкасти на українській, подкасти українським, подкасти на українском язике, подкасти слухати, подкасти на google podcasts, подкасти apple podcasts, google podcasts, apple podcasts, топ подкасти україни, цікаві подкасти, подкасти це, подкасті, найкращі українські подкасти, підкасти це, де слухати подкасти, подкасти слухати, сайт з подкастами, аудіо книги скачать, що таке подкаст, аудіо книги, google подкасти, подкасти про психологію, легкі подкасти, urban space radio, urban space 100, USR, урбан радіо" name="keywords">
   <meta content="Радіо нового формату, в ефірі якого звучить українська сучасна музика і програми на тему урбаністики та розвитку міста" name="description">
@@ -45,6 +29,25 @@ defined('ABSPATH') || exit;
            var wayforpay = new Wayforpay();
            wayforpay.invoice(url);
        }</script>
+  <!-- Facebook Pixel Code -->
+  <script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		  n.queue=[];t=b.createElement(e);t.async=!0;
+		  t.src=v;s=b.getElementsByTagName(e)[0];
+		  s.parentNode.insertBefore(t,s)}(window,document,'script',
+		  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '762186338422722');
+	  fbq('track', 'PageView');
+  </script>
+  <noscript>
+    <img height="1" width="1"
+         src="https://www.facebook.com/tr?id=762186338422722&ev=PageView
+&noscript=1"/>
+  </noscript>
+  <!-- End Facebook Pixel Code -->
   <?php wp_head(); ?>
 </head>
 <body>
